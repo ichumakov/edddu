6d7c6f5ce0f542b8be112f1ca1250c86eedc5ad4
Ilya <chumakov-il@ya.ru>
2014-08-29 22:26:41 +0400

    Add toogle

---
+++ script.js
@@ -0,0 +1,19 @@
+window.onresize = function() {
+        if (matchMedia) {
+          var mq = window.matchMedia("(max-width: 640px)");
+          mq.addListener(WidthChange);
+          WidthChange(mq);
+        }
+
+        // media query change
+        function WidthChange(mq) {
+
+          if (mq.matches) {
+            document.getElementById('toogle').style.display = 'block';
+          }
+        else {
+          document.getElementById('toogle').style.display = 'none';
+        }
+
+        }
+      }
